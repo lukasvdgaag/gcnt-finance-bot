3711e3f7734f6925e555652366040d192fe1a23c
GCNT <lukasvdgaag@gmail.com>
2023-02-15 16:33:55 +0100

    Fixed mysql to use connection pools.

--- src/repository/Repository.js
+++ src/repository/Repository.js
@@ -2,31 +2,36 @@ import mysql from "mysql";
 
 export default class Repository {
 
-    getConnection() {
-        return mysql.createConnection({
+    /**
+     * @type {Pool}
+     */
+    static pool;
+    static {
+        Repository.pool = mysql.createPool({
             host: process.env.MYSQL_HOST,
             user: process.env.MYSQL_USER,
             password: process.env.MYSQL_PASS,
-            database: process.env.MYSQL_DB
+            database: process.env.MYSQL_DB,
+            connectionLimit: 10
         });
     }
 
     async executeSQL(query, values = []) {
         return new Promise(async (ok, fail) => {
-            const connection = this.getConnection();
-            connection.connect(function (err) {
+            Repository.pool.getConnection(function (err, connection) {
                 if (err) {
+                    connection.release();
                     fail(err);
                     return;
                 }
-                connection.query(query, values,
-                    function (err, result) {
-                        if (err) {
-                            fail(err);
-                            return;
-                        }
-                        ok(result ?? null);
-                    });
+                connection.query(query, function (err, result) {
+                    connection.release();
+                    if (err) {
+                        fail(err);
+                        return;
+                    }
+                    ok(result ?? null);
+                });
             });
         });
     }
